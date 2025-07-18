9335c7865db756f3d257b8dcf17fed7fb6465eaa
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-07-18 19:52:10 +0200

    [21_1] better constructor test

--- test/jnk/laborpruefung/labor21_1/TestHelpers.java
+++ test/jnk/laborpruefung/labor21_1/TestHelpers.java
@@ -3,6 +3,7 @@ package jnk.laborpruefung.labor21_1;
 import org.junit.jupiter.api.Assertions;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class TestHelpers {
@@ -11,11 +12,49 @@ public class TestHelpers {
         Field field;
         try {
             field = klass.getDeclaredField(fieldName);
-        } catch (NoSuchFieldException e) {
+        } catch (NoSuchFieldException ignored) {
             throw new RuntimeException("This should never happen!");
         }
 
         Assertions.assertEquals(expectedType, field.getType(), String.format("Field %s is not of the expected type", fieldName));
         Assertions.assertEquals(Modifier.toString(expectedModifiers), Modifier.toString(field.getModifiers()), String.format("Field %s doesn't have the right modifiers", fieldName));
+    }
+
+    public static void checkConstructor(Class<?> klass, Class<?>[] constructorParams) {
+        Assertions.assertDoesNotThrow(() -> klass.getConstructor(constructorParams), String.format("Class %s doesn't have constructor with expected parameters", klass));
+    }
+
+    public static void checkMethod(Class<?> klass, String methodName, Class<?>[] expectedArguments, Class<?> expectedReturnType, int expectedModifiers) {
+        Assertions.assertDoesNotThrow(() -> klass.getDeclaredMethod(methodName, expectedArguments), String.format("Method %s doesn't exist", methodName));
+        Method method;
+        try {
+            method = klass.getDeclaredMethod(methodName, expectedArguments);
+        } catch (NoSuchMethodException ignored) {
+            throw new RuntimeException("This should never happen!");
+        }
+
+        Assertions.assertEquals(expectedReturnType, method.getReturnType(), String.format("Method %s doesn't have correct return type", methodName));
+        Assertions.assertEquals(Modifier.toString(expectedModifiers), Modifier.toString(method.getModifiers()), String.format("Method %s doesn't have the right modifiers", methodName));
+    }
+
+    public static <T> T getFieldValue(Class<?> klass, String fieldName, Object object) {
+        Field field = null;
+        while (klass != null) {
+            try {
+                field = klass.getDeclaredField(fieldName);
+                break;
+            } catch (NoSuchFieldException e) {
+                klass = klass.getSuperclass();
+            }
+        }
+        if (field == null) return null;
+
+        field.trySetAccessible();
+        try {
+            //noinspection unchecked
+            return (T) field.get(object);
+        } catch (IllegalAccessException | ClassCastException ignored) {
+            return null;
+        }
     }
 }
--- test/jnk/laborpruefung/labor21_1/inuit/aufgabe1/TestKiste.java
+++ test/jnk/laborpruefung/labor21_1/inuit/aufgabe1/TestKiste.java
@@ -4,12 +4,10 @@ import jnk.laborpruefung.labor21_1.TestHelpers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class TestKiste {
-    private Class<Kiste> klass = Kiste.class;
+    private final Class<Kiste> klass = Kiste.class;
 
     @Test
     public void testInhaltLeicht() {
@@ -18,41 +16,29 @@ public class TestKiste {
 
     @Test
     public void testKonstruktorLeicht() {
-        Class<?>[] constructorParams = new Class<?>[]{int.class, String.class};
-        Assertions.assertDoesNotThrow(() -> klass.getConstructor(constructorParams), "Class Kiste doesn't have constructor with expected parameters");
-        Constructor<Kiste> constructor = null;
-        try {
-            constructor = klass.getConstructor(constructorParams);
-        } catch (NoSuchMethodException ignored) {}
-        Assertions.assertNotNull(constructor);
+       TestHelpers.checkConstructor(klass, new Class<?>[]{int.class, String.class});
+
+       Kiste k = new Kiste(4, "Fish");
+
+       String realInhalt = TestHelpers.getFieldValue(klass, "inhalt", k);
+       @SuppressWarnings("DataFlowIssue")
+       int realGroesse = TestHelpers.getFieldValue(klass, "groesse", k);
+       Assertions.assertEquals("Fish", realInhalt, "Constructor does not set provided value for inhalt!");
+       Assertions.assertEquals(4, realGroesse, "Constructor does not set provided value for groesse!");
     }
 
     @Test
     public void testToStringLeicht() {
-        Assertions.assertDoesNotThrow(() -> klass.getMethod("toString"), "Method toString doesn't exist");
-        Method method = null;
-        try {
-            method = klass.getDeclaredMethod("toString");
-        } catch (NoSuchMethodException ignored) {}
-        Assertions.assertNotNull(method);
-
-        Assertions.assertTrue(Modifier.isPublic(method.getModifiers()), "Method toString is not public");
+        TestHelpers.checkMethod(klass, "toString", new Class<?>[]{}, String.class, Modifier.PUBLIC);
 
         Kiste kiste = new Kiste(4, "Fish");
         Assertions.assertEquals("Kiste mit Fish", kiste.toString());
     }
 
     @Test
-    public void testZusammenLegenLeicht() {
-        Class<?>[] methodParams = new Class<?>[]{klass};
-        Assertions.assertDoesNotThrow(() -> klass.getMethod("zusammenlegen", methodParams), "Method zusammenlegen(Kiste) doesn't exist");
-        Method method = null;
-        try {
-            method = klass.getDeclaredMethod("zusammenlegen", methodParams);
-        } catch (NoSuchMethodException ignored) {}
-        Assertions.assertNotNull(method);
+    public void testZusammenlegenLeicht() {
+        TestHelpers.checkMethod(klass, "zusammenlegen", new Class<?>[]{klass}, klass, Modifier.PUBLIC);
 
-        Assertions.assertTrue(Modifier.isPublic(method.getModifiers()), "Method toString is not public");
         // TODO: the rest of the test (check the arguments and their types)
     }
 }
